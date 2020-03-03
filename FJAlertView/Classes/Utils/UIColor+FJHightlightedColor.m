5b72510721f10d6016a35c427baee6c9131776ec
fengj <fengarenas@126.com>
2020-03-03 22:59:38 +0800

    fix(Dialog):修复ColorSpace不一致

--- FJAlertView/Classes/Utils/UIColor+FJHightlightedColor.m
+++ FJAlertView/Classes/Utils/UIColor+FJHightlightedColor.m
@@ -14,17 +14,31 @@
 @implementation UIColor (FJHightlightedColor)
 
 - (UIColor *)hightlightedColor {
+    CGFloat r;
+    CGFloat g;
+    CGFloat b;
+    CGFloat a;
+    NSUInteger numberOfComponents = CGColorGetNumberOfComponents(self.CGColor);
     const CGFloat *components = CGColorGetComponents(self.CGColor);
-    
-    CGFloat r = components[0];
-    CGFloat g = components[1];
-    CGFloat b = components[2];
+    if (numberOfComponents==4) {
+        //UIDeviceRGBColorSpace
+        r = components[0];
+        g = components[1];
+        b = components[2];
+        a = components[3];
+    } else if (numberOfComponents==2) {
+        //UIDeviceWhiteColor
+        r = components[0];
+        g = components[0];
+        b = components[0];
+        a = components[1];
+    }
     
     r = r * 255 - deepValue >= 0 ? r * 255 - deepValue : 0;
     g = g * 255 - deepValue >= 0 ? g * 255 - deepValue : 0;
     b = b * 255 - deepValue >= 0 ? b * 255 - deepValue : 0;
     
-    return [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:components[3]];
+    return [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a];
 }
 
 @end
